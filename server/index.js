2100638329083ceb3e61a795d5dbb09a66ed2084
oluwaseunalo <oluwaseunalo4@gmail.com>
2021-12-12 04:42:02 +0100

    database: Establish connection with Mongodb atlas database

--- server/index.js
+++ server/index.js
@@ -2,9 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const App = express();
+const app = express();
+dotenv.config();
 
-App.use(bodyParser.json({ limit: "30mb", extended: true }));
-App.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-App.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`The server is running on port ${PORT}`);
+    })
+  )
+  .catch((error) => console.log(error.message));
