74b4b945d6a3f97e742ca732ed560ff16e534b91
oluwaseunalo <oluwaseunalo4@gmail.com>
2021-12-13 01:53:51 +0100

    postRoute: Setting up the get post routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { getPosts } from "../controller/posts.js";
+import { getPosts, createPost } from "../controller/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/", createPost);
 
 export default router;
