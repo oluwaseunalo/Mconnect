e132c245b94658d05f000ecac0e9a2958d4bcb02
oluwaseunalo <oluwaseunalo4@gmail.com>
2021-12-26 23:17:56 +0100

    meta: Working on the meta data in the index.html

--- client/src/components/Posts/styles.js
+++ client/src/components/Posts/styles.js
@@ -1,9 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-  mainContainer: {
+  container: {
     display: "flex",
     alignItems: "center",
+    marginTop: "3%",
   },
   smMargin: {
     margin: theme.spacing(1),
